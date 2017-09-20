a39077eabbc32b913a91b8df9f87b2c30d65a35e
Emily Roycroft <emily.roycroft@gmail.com>
2017-09-20 13:15:37 +1000

    Add files via upload

---
+++ Biodependencies.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+####################################################
+#                                                  #
+# Useful dependencies for biologist's using Linux  #
+# EMILY ROYCROFT                                   #
+#                                                  #
+####################################################
+
+
+# convert text file line endings between CRLF and LF
+
+sudo apt install dos2unix 
+
+# command line trashcan utility 
+
+sudo apt install trash-cli
+
+# Python libray for bioinformatics (implemented in Python 2)
+
+sudo apt install python-biopython 
+
+
+
+####################################################
+#                                                  #
+# To look for new packages available through the   #
+# ADVANCED PACKAGE TOOL (apt) use the code         #
+# sudo apt-cache search <search term>              #
+#                                                  #
+####################################################
+
